35d5cef709bb105ac325ec81c2cae4bb408e12cc
Daniel Santos <danielandsanfer@gmail.com>
2026-09-01 21:26:52 -0300

    catualization

--- app/main.py
+++ app/main.py
@@ -1,12 +1,46 @@
+from http.client import HTTPException
+
 from fastapi import FastAPI
+
+Livros = [
+    "O Senhor dos Anéis",
+    "Harry Potter",
+    "O Pequeno Príncipe",
+    "1984",
+    "O Hobbit"
+]
 
 app = FastAPI()
 
-@app.get("/")
-async def home():
-    return {"message": "Hello, World!"}
 
-
+# Saudação
 @app.get("/saudacao")
-async def saudacao(string: str):
-    return {"message": f"Olá, {string}! Seja bem-vindo!"}
+async def saudacao():
+    return {"message": "Bem vindo à API de Livros!"}
+
+
+# Listar livros
+@app.get("/livros")
+async def listar_livros():
+    return {"livros": Livros}
+
+
+# Adicionar livro
+@app.post("/livros")
+async def adicionar_livro(livro: str):
+    Livros.append(livro)
+    return {"message": "Livro adicionado com sucesso!"}
+
+@app.put("/livros/{index}")
+async def atualizar_livro(index: int, new_livro: str):
+    if index > len(Livros) or index < 0:
+        raise HTTPException(status_code=404, detail="Livro não encontrado")
+    Livros[index] = new_livro
+    return {"message": "Livro atualizado com sucesso!"}
+
+@app.delete("/livros/{index}")
+async def deletar_livro(index: int):
+    if index > len(Livros) or index < 0:
+        raise HTTPException(status_code=404, detail="Livro não encontrado") 
+    Livros.pop(index)
+    return {"message": "Livro deletado com sucesso!"}
